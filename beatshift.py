c0dd9511c0fdd19e69aacf0da4f66ee90238bb32
mulfordjc <mulfordjc@users.noreply.github.com>
2015-04-04 23:45:31 -0400

    Pushed dependencies for song_mix_loopback.py

---
+++ beatshift.py
@@ -0,0 +1,57 @@
+"""
+beatshift.py
+
+Shifts the beat of a file over a given period of time by a given magnitude at the given interval
+
+Created by Jacob Mulford on 2/18/2015
+
+Based on https://github.com/echonest/remix/blob/master/examples/stretch/simple_stretch.py
+
+Updated on 04/04/2015 to be a method called by another script
+"""
+import dirac
+from echonest.remix import audio
+from pyechonest import track
+
+usage = """
+Usage: python beatshift.py <input_filename> <start_beat> <shift_length> <shift_magnitude> <output_filename>
+
+Example: python beatshift.py CallMeMaybe.mp3 10 50 -10 CallMeShift.mp3
+
+This will shift the tempo of CallMeMaybe.mp3 by -10 bpm over 50 beats starting at beat 10.
+"""
+
+def tempo_shift(input_filename, seg_range, shift_length, second_song):
+    t1 = track.track_from_filename(input_filename)
+    t2 = track.track_from_filename(second_song)
+
+    start_range, end_range = seg_range
+
+    shift_length = min(shift_length, end_range - start_range)
+    shift_magnitude = t2.tempo - t1.tempo
+
+    beat_increment = (1.0*shift_magnitude)/shift_length
+    beat_ratio = 1.0
+    beat_count = 0
+
+    audiofile = audio.LocalAudioFile(input_filename)
+    beats = audiofile.analysis.segments
+    collect = []
+
+    for i in range(start_range, end_range):
+        if (i > (end_range - shift_length)):
+            desired_bpm = beat_increment * (i - (end_range - shift_length)) + t1.tempo
+            beat_ratio = t1.tempo/desired_bpm
+
+        beat_audio = beats[i].render()
+
+        if (beat_ratio == 1.0):
+            collect.append(beat_audio)
+        else:
+            scaled_beat = dirac.timeScale(beat_audio.data, beat_ratio)
+            ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape,
+                    sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
+            collect.append(ts)
+
+    return collect
+
---
+++ twosongshift.py
@@ -0,0 +1,81 @@
+"""
+twosongshift.py
+
+Takes 2 songs and determines the ideal transition between them if tempo is ignored.
+
+Created by Jacob Mulford on 3/01/2015
+
+Based on infinitejuke.com
+
+This script was altered on 04/04/2015 to be called by another class, and returns the ideal transition instead of writing it to a file
+"""
+import math
+import sys
+import pyechonest.track as track
+
+usage = """
+Usage: python twosongshift.py <first_filename> <second_filename> <ratio> <output_file>
+
+Example: python twosongshift.py CallMeMaybe.mp3 ShakeItOff.mp3 .33 Transitions.txt
+
+This will determine the transitions between the segments .33 of each side from the middle of CallMeMaybe.mp3 and ShakeItOff.mp3, and will put the
+best transition into Transitions.txt.  Transitions.txt will hold 2 integers.
+The first integer will be the segment in CallMeMaybe.mp3, and the second integer will be the
+segment in ShakeItOff.mp3.
+"""
+
+#replaced main
+def get_transition(first_filename, second_filename, ratio):
+    #set up the 2 files for analysis
+    track_one = track.track_from_filename(first_filename)
+    track_one.get_analysis()
+
+    track_two = track.track_from_filename(second_filename)
+    track_two.get_analysis()
+
+    if (ratio > 1.0 or ratio < 0.0):
+        print "Error: ratio must be between 0.0 and 1.0"
+        sys.exit(-1)
+
+    first_middle = len(track_one.segments)/2
+    second_middle = len(track_two.segments)/2
+
+    first_start = int(first_middle - (first_middle * ratio))
+    first_end = int(first_middle + (first_middle * ratio))
+
+    second_start = int(second_middle - (second_middle * ratio))
+    second_end = int(second_middle + (second_middle * ratio))
+
+    #compare each segment in the first file to each segment in the second file
+    comparisons = []
+    for i in range(first_start,first_end):
+        appender = []
+        for j in range(second_start,second_end):
+            compare = compare_segments(track_one.segments[i],track_two.segments[j])
+            appender.append(compare)
+        comparisons.append(appender)
+
+    (first_low,second_low) = (0,0)
+
+    for i in range(0,len(comparisons)):
+        for j in range(0,len(comparisons[i])):
+            if comparisons[i][j] < comparisons[first_low][second_low]:
+                first_low = i
+                second_low = j
+
+    return (first_low+first_start,second_low+second_start)
+
+#determines the weighted Euclidean distance between 2 segments
+def compare_segments(seg_one, seg_two):
+    timbre_distance = euc_dist(seg_one['timbre'],seg_two['timbre'])
+    pitch_distance = euc_dist(seg_one['pitches'],seg_two['pitches'])
+    loud_distance = (seg_one['loudness_start'] - seg_two['loudness_start'])**2
+    return timbre_distance + 10*pitch_distance + loud_distance
+
+#calculates euclidean distance
+def euc_dist(arr_one,arr_two):
+    sum = 0
+    for i in range(0,min(len(arr_one),len(arr_two))):
+        sum = sum + (arr_one[i] - arr_two[i])**2
+    return math.sqrt(sum)
+
